6db01e87e07a8edc63a6204fbd81037b2fe18c48
albiremo <albertoremigi92@gmail.com>
2016-10-18 15:44:30 +0200

    Second commit

--- hm.py
+++ hm.py
@@ -13,10 +13,8 @@ from   numpy   import pi
 def NACA4camberline(xc,mc,pos_mc):
     m=pos_mc/100
     p=pos_mc/10
-    yc=np.zeros(xc.shape)
-    Dyc=np.zeros(xc.shape)
-    yc = ((m/p**2)*(2*p - xc)*xc)*(xc<p) + ((m/(1-p)**2)*(1- 2*p + (2*p - xc)*xc))*(xc>=p)
-    Dyc = ((m/p**2)*2*(p-xc))*(xc<p) + ((m/(1-p)**2) * 2*(p-xc))*(xc>=p)
+    yc = ((m/(p**2))*(2*p-xc)*xc)*(xc<p)+((m/(1-p)**2)*(1- 2*p + (2*p-xc)*xc))*(xc>=p)
+    Dyc = ((m/p**2)*2*(p-xc))*(xc<p)+((m/(1-p)**2) * 2*(p-xc))*(xc>=p)
     return yc,Dyc
 
 def NACAthick(xc,SS,xtec):
@@ -37,13 +35,15 @@ def NACAthick(xc,SS,xtec):
          0.28430*xc**3 -0.10150*xc**4)*(xc<xtec) +\
         5*s*(0.29690*np.sqrt(xc)+(-0.12600+b[0])*xc +(-0.35160+b[1])*xc**2 \
         + (0.28430+b[2])*xc**3 +(-0.10150+b[3])*xc**4)*(xc>=xtec)
+    tk[len(tk)-1]=0
     return tk
 
 
 
 def NACA(code,nc):
+    nc=nc+1
     xc=np.zeros([nc],dtype=np.float64)
-    nc=nc+1 #I've to make 1 more every time, because the last one of arange
+     #I've to make 1 more every time, because the last one of arange
     #is not complained
     xc=0.5*(1-np.cos(np.arange(0,pi,pi/(nc-1))))
     nv=2*nc-1 #number of panel vertices, must be double of number of nodes but
@@ -61,28 +61,36 @@ def NACA(code,nc):
             pos_mc=np.float64(A[1])     #position of maximum camber
             SS=np.float64(A[2])*10+np.float64(A[3])
             print('max_camber:',mc,'pos_max_camber:',pos_mc,'max_thick:',SS)
-            xtec=np.float64(0.98)
+            xtec=np.float64(1)
              #maximum thickness
             # camber line construction
             yc,Dyc=NACA4camberline(xc,mc,pos_mc)
 
             #thickness construction
             tk=NACAthick(xc,SS,xtec)
-
+            print(xv[0:nc-1].shape,xv[nc:nv-1].shape)
             theta=np.arctan(Dyc)
-            xv=np.zeros([nv],np.float64)
-            yv=np.zeros([nv],np.float64)
-            xv[0:nc-1]=xc-tk*np.sin(theta)
-            yv[0:nc-1]=yc+tk*np.cos(theta)
-            xv[nc:nv-1]=xc[1:nc-1]+tk[1:nc-1]+np.cos(theta[1:nc-1])
-            yv[nc:nv-1]=yc[1:nc-1]-tk[1:nc-1]+np.cos(theta[1:nc-1])
+            print(tk.shape, theta.shape,xc.shape,nv)
+            #xv=np.zeros([nv],np.float64)
+            #yv=np.zeros([nv],np.float64)
+            xv[0:nc-1]=xc[0:nc-1]-tk*np.sin(theta[0:nc-1])
+            yv[0:nc-1]=yc[0:nc-1]+tk*np.cos(theta[0:nc-1])
+            xv[0:nc-1]=xv[nc-1:0:-1]
+            yv[0:nc-1]=yv[nc-1:0:-1]
+            xv[nc:nv-1]=xc[1:nc-1]+tk[1:nc-1]*np.sin(theta[1:nc-1])
+            yv[nc:nv-1]=yc[1:nc-1]-tk[1:nc-1]*np.cos(theta[1:nc-1])
+            with open("naca_airfoil.txt","w") as air:
+                for i in range(1,nv-1):
+                    print(xv[i],yv[i], file=air, sep=' ')
 
             #we choose anticlockwise direction from top to bottom
-            xv=xv[::-1]
-            yv=yv[::-1]
+            plt.figure()
+            plt.plot(xv,yv,'-*')
+            plt.axis('equal')
+            plt.show()
             return(xv,yv)
         else:
             #TODO NACA 5 DIGIT
             return(yv)
 
-print(NACA('2315',100))
+print(NACA('2315',50))
